ea24e1f7d8e2e7b453b2cbd081bbd1adc423f3a4
astandro <astandro.tc@gmail.com>
2014-07-01 12:27:47 +0700

    done

--- FakturPajakApp/FakturPajakApp/Form1.Designer.cs
+++ FakturPajakApp/FakturPajakApp/Form1.Designer.cs
@@ -54,11 +54,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.label1.Location = new System.Drawing.Point(215, 9);
+            this.label1.Location = new System.Drawing.Point(273, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 21);
+            this.label1.Size = new System.Drawing.Size(214, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "Input Faktur Pajak";
             // 
@@ -66,7 +66,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(89, 121);
+            this.label2.Location = new System.Drawing.Point(187, 122);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(108, 17);
             this.label2.TabIndex = 1;
@@ -76,7 +76,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(89, 163);
+            this.label3.Location = new System.Drawing.Point(187, 164);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(119, 17);
             this.label3.TabIndex = 2;
@@ -86,7 +86,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(89, 243);
+            this.label4.Location = new System.Drawing.Point(187, 330);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(59, 17);
             this.label4.TabIndex = 3;
@@ -96,7 +96,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(89, 202);
+            this.label5.Location = new System.Drawing.Point(187, 203);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(62, 17);
             this.label5.TabIndex = 4;
@@ -106,7 +106,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(89, 282);
+            this.label6.Location = new System.Drawing.Point(187, 369);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(33, 17);
             this.label6.TabIndex = 5;
@@ -116,7 +116,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(89, 360);
+            this.label7.Location = new System.Drawing.Point(187, 447);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(55, 17);
             this.label7.TabIndex = 6;
@@ -126,7 +126,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(89, 322);
+            this.label8.Location = new System.Drawing.Point(187, 409);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(69, 17);
             this.label8.TabIndex = 7;
@@ -134,52 +134,54 @@
             // 
             // tbxNoFP1
             // 
-            this.tbxNoFP1.Location = new System.Drawing.Point(290, 121);
+            this.tbxNoFP1.Location = new System.Drawing.Point(388, 122);
             this.tbxNoFP1.Name = "tbxNoFP1";
             this.tbxNoFP1.Size = new System.Drawing.Size(114, 20);
-            this.tbxNoFP1.TabIndex = 8;
+            this.tbxNoFP1.TabIndex = 2;
             // 
             // tbxNamaPerusahaan
             // 
-            this.tbxNamaPerusahaan.Location = new System.Drawing.Point(290, 163);
+            this.tbxNamaPerusahaan.Location = new System.Drawing.Point(388, 164);
             this.tbxNamaPerusahaan.Name = "tbxNamaPerusahaan";
             this.tbxNamaPerusahaan.Size = new System.Drawing.Size(224, 20);
-            this.tbxNamaPerusahaan.TabIndex = 9;
+            this.tbxNamaPerusahaan.TabIndex = 4;
             // 
             // tbxDeskripsi
             // 
-            this.tbxDeskripsi.Location = new System.Drawing.Point(290, 202);
+            this.tbxDeskripsi.Location = new System.Drawing.Point(388, 203);
+            this.tbxDeskripsi.Multiline = true;
             this.tbxDeskripsi.Name = "tbxDeskripsi";
-            this.tbxDeskripsi.Size = new System.Drawing.Size(224, 20);
-            this.tbxDeskripsi.TabIndex = 10;
+            this.tbxDeskripsi.Size = new System.Drawing.Size(224, 107);
+            this.tbxDeskripsi.TabIndex = 5;
             // 
             // tbxNominal
             // 
-            this.tbxNominal.Location = new System.Drawing.Point(290, 240);
+            this.tbxNominal.Location = new System.Drawing.Point(388, 327);
             this.tbxNominal.Name = "tbxNominal";
             this.tbxNominal.Size = new System.Drawing.Size(224, 20);
-            this.tbxNominal.TabIndex = 11;
+            this.tbxNominal.TabIndex = 6;
             // 
             // tbxDPP
             // 
-            this.tbxDPP.Location = new System.Drawing.Point(290, 282);
+            this.tbxDPP.Location = new System.Drawing.Point(388, 369);
             this.tbxDPP.Name = "tbxDPP";
             this.tbxDPP.Size = new System.Drawing.Size(224, 20);
-            this.tbxDPP.TabIndex = 12;
+            this.tbxDPP.TabIndex = 7;
+            this.tbxDPP.TextChanged += new System.EventHandler(this.tbxDPP_TextChanged);
             // 
             // tbxPPN
             // 
-            this.tbxPPN.Location = new System.Drawing.Point(290, 322);
+            this.tbxPPN.Location = new System.Drawing.Point(388, 409);
             this.tbxPPN.Name = "tbxPPN";
             this.tbxPPN.Size = new System.Drawing.Size(224, 20);
-            this.tbxPPN.TabIndex = 13;
+            this.tbxPPN.TabIndex = 8;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(290, 360);
+            this.dateTimePicker1.Location = new System.Drawing.Point(388, 447);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(224, 20);
-            this.dateTimePicker1.TabIndex = 14;
+            this.dateTimePicker1.TabIndex = 9;
             this.dateTimePicker1.Value = new System.DateTime(2014, 7, 1, 10, 30, 24, 0);
             // 
             // btnSubmit
@@ -191,33 +193,34 @@
             this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSubmit.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSubmit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnSubmit.Location = new System.Drawing.Point(219, 402);
+            this.btnSubmit.Location = new System.Drawing.Point(317, 489);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(168, 48);
-            this.btnSubmit.TabIndex = 15;
+            this.btnSubmit.TabIndex = 10;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
             // btnBrowse
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(92, 49);
+            this.btnBrowse.Location = new System.Drawing.Point(190, 50);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Size = new System.Drawing.Size(105, 23);
-            this.btnBrowse.TabIndex = 17;
+            this.btnBrowse.TabIndex = 0;
             this.btnBrowse.Text = "Browse Report";
             this.btnBrowse.UseVisualStyleBackColor = true;
             this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
             // tbxNoFP2
             // 
-            this.tbxNoFP2.Location = new System.Drawing.Point(410, 121);
+            this.tbxNoFP2.Location = new System.Drawing.Point(508, 122);
             this.tbxNoFP2.Name = "tbxNoFP2";
             this.tbxNoFP2.Size = new System.Drawing.Size(104, 20);
-            this.tbxNoFP2.TabIndex = 18;
+            this.tbxNoFP2.TabIndex = 3;
             // 
             // tbxPathReport
             // 
-            this.tbxPathReport.Location = new System.Drawing.Point(290, 51);
+            this.tbxPathReport.Location = new System.Drawing.Point(388, 52);
             this.tbxPathReport.Name = "tbxPathReport";
             this.tbxPathReport.ReadOnly = true;
             this.tbxPathReport.Size = new System.Drawing.Size(224, 20);
@@ -226,7 +229,7 @@
             // 
             // tbxPathTemplate
             // 
-            this.tbxPathTemplate.Location = new System.Drawing.Point(290, 77);
+            this.tbxPathTemplate.Location = new System.Drawing.Point(388, 78);
             this.tbxPathTemplate.Name = "tbxPathTemplate";
             this.tbxPathTemplate.ReadOnly = true;
             this.tbxPathTemplate.Size = new System.Drawing.Size(224, 20);
@@ -235,10 +238,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(92, 75);
+            this.button1.Location = new System.Drawing.Point(190, 76);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(105, 23);
-            this.button1.TabIndex = 20;
+            this.button1.TabIndex = 1;
             this.button1.Text = "Browse Template";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -247,7 +250,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 486);
+            this.ClientSize = new System.Drawing.Size(802, 557);
             this.Controls.Add(this.tbxPathTemplate);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tbxPathReport);
--- FakturPajakApp/FakturPajakApp/Form1.cs
+++ FakturPajakApp/FakturPajakApp/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Excel=Microsoft.Office.Interop.Excel;
 
 namespace FakturPajakApp
 {
     public partial class Form1 : Form
     {
+        public string templatePath;
         public Form1()
         {
             InitializeComponent();
@@ -23,7 +26,7 @@ namespace FakturPajakApp
             fdlg.Title = "Browse Faktur Pajak";
             fdlg.InitialDirectory = @"c:\";
             fdlg.Filter = "MS excel files (*.xls)|*.xls|MS excel 2010++ files (*.xlsx)|*.xlsx";
-            fdlg.FilterIndex = 2;
+            fdlg.FilterIndex = 1;
             fdlg.RestoreDirectory = true;
             if (fdlg.ShowDialog() == DialogResult.OK)
             {
@@ -37,14 +40,79 @@ namespace FakturPajakApp
             fdlg.Title = "Browse Faktur Pajak";
             fdlg.InitialDirectory = @"c:\";
             fdlg.Filter = "MS excel files (*.xls)|*.xls|MS excel 2010++ files (*.xlsx)|*.xlsx";
-            fdlg.FilterIndex = 2;
+            fdlg.FilterIndex = 1;
             fdlg.RestoreDirectory = true;
             if (fdlg.ShowDialog() == DialogResult.OK)
             {
                 tbxPathTemplate.Text = fdlg.FileName;
+                templatePath = fdlg.FileName;
             }
         }
 
-        public 
+        public void newFakturPajak()
+        {
+            string savePath= "c:\\";
+
+            SaveFileDialog sfdlg = new SaveFileDialog();
+            sfdlg.InitialDirectory = @"c:\";
+            sfdlg.Filter = "MS excel files (*.xls)|*.xls|MS excel 2010++ files (*.xlsx)|*.xlsx";
+            sfdlg.FilterIndex = 1;
+            sfdlg.RestoreDirectory = true;
+            if (sfdlg.ShowDialog() == DialogResult.OK)
+            {
+                savePath = sfdlg.FileName;
+            }
+
+            Excel.Workbook MyBook = null;
+            Excel.Application MyApp = null;
+            Excel.Worksheet MySheet = null;
+
+            MyApp = new Excel.Application();
+            MyApp.Visible = false;
+            MyBook = MyApp.Workbooks.Open(templatePath);
+            MySheet = (Excel.Worksheet)MyBook.Sheets[1];
+
+
+            MySheet.Cells[4, 7] = tbxNoFP1.ToString() + "-" + tbxNoFP2.ToString();
+            MySheet.Cells[11, 6] = tbxNamaPerusahaan.Text.ToString();
+            MySheet.Cells[19, 4] = tbxDeskripsi.Lines.ElementAt(0).ToString();
+            MySheet.Cells[20, 4] = tbxDeskripsi.Lines.ElementAt(1).ToString();
+            MySheet.Cells[21, 4] = tbxDeskripsi.Lines.ElementAt(2).ToString();
+            MySheet.Cells[22, 4] = tbxDeskripsi.Lines.ElementAt(3).ToString();
+            MySheet.Cells[19, 9] = tbxNominal.Text.ToString();
+            MySheet.Cells[36, 9] = tbxDPP.Text.ToString();
+            MySheet.Cells[37, 9] = tbxPPN.Text.ToString();
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "MM/dd/yyyy";
+
+            MySheet.Cells[40, 10] = dateTimePicker1.ToString();
+            try
+            {
+                MyBook.SaveAs(savePath);
+                MyBook.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void tbxDPP_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                tbxPPN.Text = (Double.Parse(tbxDPP.Text.ToString()) * 0.1).ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            newFakturPajak();
+        }
     }
 }
